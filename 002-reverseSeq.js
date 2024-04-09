e793108a7a8fecb5f52b4d325f0c6ebe5ee846a9
L~ <leonardowijaya85@gmail.com>
2024-04-09 14:51:12 +0700

    challenge 002/366

---
+++ 002-reverseSeq.js
@@ -0,0 +1,28 @@
+// WPU Coding Challenge 2024
+// 002/366
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+// const reverseSeq = (n) => {
+//   const result = [];
+//   for (let i = n; i >= 1; i--) {
+//     result.push(i);
+//   }
+//   return result;
+// };
+
+// reverseSeq = (n) => {
+//   return Array(n)
+//     .fill()
+//     .map((l, m) => m + 1)
+//     .reverse();
+// };
+
+// reverseSeq = (n) => {
+//   return [...Array(n)].map((l, m) => m + 1).reverse();
+// };
+
+const reverseSeq = (n) => [...Array(n)].map((l, m) => m + 1).reverse();
+
+// const reverseSeq = (n) => [...Array(n)].map((l, m) => n - m);
+
+console.log(reverseSeq(5));
