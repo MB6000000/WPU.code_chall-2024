18c7d8c3fef2037303860933469dd386f97d01ee
L~ <leonardowijaya85@gmail.com>
2024-04-10 11:43:04 +0700

    challenge 006/366

--- 006-DNAtoRNA.js
+++ 006-DNAtoRNA.js
@@ -1,6 +1,18 @@
 // WPU Coding Challenge 2024
-// 6/366
+// 006/366
 // https://www.codewars.com/kata/5556282156230d0e5e000089
+
+// function DNAtoRNA(dna) {
+//   let rna = '';
+//   for (let i = 0; i < dna.length; i++) {
+//     if (dna[i] === 'T') {
+//         rna += 'U';
+//     } else {
+//         rna += dna[i];
+//     }
+//   }
+//   return rna;
+// }
 
 // function DNAtoRNA(dna) {
 //   let rna = '';
@@ -9,6 +21,13 @@
 //   }
 //   return rna;
 // }
+
+function DNAtoRNA(dna) {
+  return dna
+    .split("")
+    .map((el) => (el === "T" ? "U" : el))
+    .join("");
+}
 
 // const DNAtoRNA = (dna) =>
 //   dna
@@ -19,6 +38,6 @@
 // const DNAtoRNA = (dna) => dna.replace(/T/g, 'U');
 // const DNAtoRNA = (dna) => dna.replaceAll('T', 'U');
 
-const DNAtoRNA = (dna) => dna.split('T').join('U');
+// const DNAtoRNA = (dna) => dna.split('T').join('U');
 
-console.log(DNAtoRNA('GCAT'));
+console.log(DNAtoRNA("GCAT"));
