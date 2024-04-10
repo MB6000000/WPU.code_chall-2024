6be45e7dfba13978c983f03c54056c985b4707a0
L~ <leonardowijaya85@gmail.com>
2024-04-10 12:06:40 +0700

    challenge 007/366

--- 007-countBy.js
+++ 007-countBy.js
@@ -1,8 +1,8 @@
 // WPU Coding Challenge 2024
-// 7/366
+// 007/366
 // https://www.codewars.com/kata/5513795bd3fafb56c200049e
 
-// function countBy(x, n) {
+// function countBy(x, y) {
 //   let z = [];
 //   for (let i = 1; i <= n; i++) {
 //     z.push(i * x);
@@ -10,9 +10,9 @@
 //   return z;
 // }
 
-// function countBy(x, n) {
-//   return [...Array(n)].map((el, i) => (i + 1) * x);
-// }
+function countBy(x, y) {
+  return [...Array(y)].map((el, i) => (i + 1) * x);
+}
 
-const countBy = (x, n) => [...Array(n)].map((_, i) => (i + 1) * x);
+// const countBy = (x, y) => [...Array(y)].map((_, i) => (i + 1) * x);
 console.log(countBy(3, 3));
